9739b7613ced8fa0db3632fb39d502f007437f3a
Jacob Hilker <jacob.hilker2@gmail.com>
2022-06-29 18:30:48 -0400

    started setting up emacs but readonly error

--- nessus/home.nix
+++ nessus/home.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+
+  imports = [ ./packages.nix ];
+
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "jhilker";
